089f2b8b7d6663df946b969e75e422dbce7aaee6
Noa Lea <noa@noalea.work>
2025-08-31 13:12:45 -0400

    Replace DM font with Lora for elegancy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { DM_Sans, DM_Mono } from "next/font/google";
+import { Lora } from "next/font/google";
 import "./globals.css";
 
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
+const lora = Lora({
+  variable: "--font-lora",
   subsets: ["latin"],
-});
-
-const dmMono = DM_Mono({
-  variable: "--font-dm-mono",
-  subsets: ["latin"],
-  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dmSans.variable} ${dmMono.variable} antialiased`}>
-        {children}
-      </body>
+      <body className={`${lora.variable} antialiased`}>{children}</body>
     </html>
   );
 }
